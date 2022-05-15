e7d82bfdc2f12fe12662c8dd9ed977fe5a5fd27b
blasscode <raafaeltrujillo@gmail.com>
2022-05-15 17:37:23 -0300

    first commit

---
+++ src/components/Nav/Bar/NavBar.js
@@ -0,0 +1,14 @@
+import'./NavBar.css'
+const NavBar = () => {
+    return(
+        <nav>
+            <h1>Ecommerce</h1>
+            <div>
+                <button> Fantasia </button>
+                <button> Fantasia </button>
+                <button> Fantasia </button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
